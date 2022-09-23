74a4b62778478e476f0bb13a74fa252df6bee656
Salmi Konsta S <kxsalmi@cubbli20-gold.local>
2022-09-23 15:43:58 +0300

    4.4, 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,40 @@ const dummy = (blogs) => {
     return 1
   }
   
+
+const totalLikes = (blogs) => {
+    var totalLikes = 0
+
+    blogs.map(blog => {
+      totalLikes = (totalLikes + blog.likes)
+    })
+  
+    return totalLikes
+}
+
+const favoriteBlog = (blogs) => {
+  var mostLiked = {
+    title: "", 
+    author: "",
+    likes: 0  
+  }
+
+  blogs.map(blog =>{
+    if (blog.likes > mostLiked.likes){
+      mostLiked = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+    }
+  })
+
+  return mostLiked
+
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
