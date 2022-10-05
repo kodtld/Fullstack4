321c03ef9fb69775b6ed8bbf66f8e724f6a49df0
Salmi Konsta S <kxsalmi@cubbli20-gold.local>
2022-10-05 16:49:40 +0300

    4.20 hehe

--- utils/middleware.js
+++ utils/middleware.js
@@ -28,8 +28,18 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = async (request, response, next) => {
+  const authorization = await request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const token = authorization.substring(7);
+    request.token = token;
+  }
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
